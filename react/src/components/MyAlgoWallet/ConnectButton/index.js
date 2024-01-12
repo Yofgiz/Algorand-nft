3214382c0bf8d6798f0a0da60b388ed05a57ecf3
Yofgiz <yoftahegizaws@gmail.com>
2024-01-12 21:24:34 +0300

    frontend with react

---
+++ react/src/components/MyAlgoWallet/ConnectButton/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "../../Button.styles";
+
+const ConnectButton = ({connectWallet}) => {
+    return(
+        <Button onClick ={connectWallet}>
+           Connect Myalgo Wallet
+        </Button>
+    )
+}
+
+export default ConnectButton
